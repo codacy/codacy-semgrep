979d2b977e58a621f145721fb475f41d6ab58029
Rafael Cortês <mrfyda@gmail.com>
2023-11-09 12:11:10 +0000

    review comments

--- internal/docgen/parsing_test.go
+++ internal/docgen/parsing_test.go
@@ -4,28 +4,28 @@ import "testing"
 
 func TestPrefixRuleIDWithPath(t *testing.T) {
 	tests := []struct {
-		name         string
+		testcase     string
 		relativePath string
 		unprefixedID string
-		want         string
+		expected     string
 	}{
 		{
-			name:         "example 1",
+			testcase:     "example 1",
 			relativePath: "apex/lang/best-practice/ncino/accessModifiers/GlobalAccessModifiers.yaml",
 			unprefixedID: "global-access-modifiers",
-			want:         "apex.lang.best-practice.ncino.accessmodifiers.globalaccessmodifiers.global-access-modifiers",
+			expected:     "apex.lang.best-practice.ncino.accessmodifiers.globalaccessmodifiers.global-access-modifiers",
 		},
 		{
-			name:         "example 2",
+			testcase:     "example 2",
 			relativePath: "javascript/lang/best-practice/leftover_debugging.yaml",
 			unprefixedID: "javascript-alert",
-			want:         "javascript.lang.best-practice.leftover_debugging.javascript-alert",
+			expected:     "javascript.lang.best-practice.leftover_debugging.javascript-alert",
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := prefixRuleIDWithPath(tt.relativePath, tt.unprefixedID); got != tt.want {
-				t.Errorf("prefixRuleIDWithPath() = %v, want %v", got, tt.want)
+		t.Run(tt.testcase, func(t *testing.T) {
+			if got := prefixRuleIDWithPath(tt.relativePath, tt.unprefixedID); got != tt.expected {
+				t.Errorf("prefixRuleIDWithPath() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
@@ -33,25 +33,25 @@ func TestPrefixRuleIDWithPath(t *testing.T) {
 
 func TestGetLastSegment(t *testing.T) {
 	tests := []struct {
-		name string
-		s    string
-		want string
+		testcase string
+		input    string
+		expected string
 	}{
 		{
-			name: "single segment",
-			s:    "insecure-use-string-copy-fn",
-			want: "insecure-use-string-copy-fn",
+			testcase: "single segment",
+			input:    "insecure-use-string-copy-fn",
+			expected: "insecure-use-string-copy-fn",
 		},
 		{
-			name: "multiple segments",
-			s:    "c.lang.security.insecure-use-string-copy-fn.insecure-use-string-copy-fn",
-			want: "insecure-use-string-copy-fn",
+			testcase: "multiple segments",
+			input:    "c.lang.security.insecure-use-string-copy-fn.insecure-use-string-copy-fn",
+			expected: "insecure-use-string-copy-fn",
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getLastSegment(tt.s); got != tt.want {
-				t.Errorf("getLastSegment() = %v, want %v", got, tt.want)
+		t.Run(tt.testcase, func(t *testing.T) {
+			if got := getLastSegment(tt.input); got != tt.expected {
+				t.Errorf("getLastSegment() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
@@ -59,35 +59,35 @@ func TestGetLastSegment(t *testing.T) {
 
 func TestGetFirstSentence(t *testing.T) {
 	tests := []struct {
-		name string
-		s    string
-		want string
+		testcase string
+		input    string
+		expected string
 	}{
 		{
-			name: "single sentence",
-			s:    "Finding triggers whenever there is a strcpy or strncpy used.",
-			want: "Finding triggers whenever there is a strcpy or strncpy used.",
+			testcase: "single sentence",
+			input:    "Finding triggers whenever there is a strcpy or strncpy used.",
+			expected: "Finding triggers whenever there is a strcpy or strncpy used.",
 		},
 		{
-			name: "multiple sentences",
-			s:    "Finding triggers whenever there is a strcpy or strncpy used. This is an issue because strcpy does not affirm the size of the destination array and strncpy will not automatically NULL-terminate strings. This can lead to buffer overflows, which can cause program crashes and potentially let an attacker inject code in the program. Fix this by using strcpy_s instead (although note that strcpy_s is an optional part of the C11 standard, and so may not be available).",
-			want: "Finding triggers whenever there is a strcpy or strncpy used.",
+			testcase: "multiple sentences",
+			input:    "Finding triggers whenever there is a strcpy or strncpy used. This is an issue because strcpy does not affirm the size of the destination array and strncpy will not automatically NULL-terminate strings. This can lead to buffer overflows, which can cause program crashes and potentially let an attacker inject code in the program. Fix this by using strcpy_s instead (although note that strcpy_s is an optional part of the C11 standard, and so may not be available).",
+			expected: "Finding triggers whenever there is a strcpy or strncpy used.",
 		},
 		{
-			name: "sentence with abbreviation or code",
-			s:    "Finding triggers whenever there is a str.cpy or strncpy used.",
-			want: "Finding triggers whenever there is a str.cpy or strncpy used.",
+			testcase: "sentence with abbreviation or code",
+			input:    "Finding triggers whenever there is a str.cpy or strncpy used.",
+			expected: "Finding triggers whenever there is a str.cpy or strncpy used.",
 		},
 		{
-			name: "sentence longer than 500 characters",
-			s:    "Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies.",
-			want: "Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candi",
+			testcase: "sentence longer than 500 characters",
+			input:    "Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies.",
+			expected: "Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candies are more tasty than orange candies, Blue candi",
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getFirstSentence(tt.s); got != tt.want {
-				t.Errorf("getFirstSentence() = %v, want %v", got, tt.want)
+		t.Run(tt.testcase, func(t *testing.T) {
+			if got := getFirstSentence(tt.input); got != tt.expected {
+				t.Errorf("getFirstSentence() = %v, expected %v", got, tt.expected)
 			}
 		})
 	}
