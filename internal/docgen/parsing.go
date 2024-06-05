c1120fd292341cbea9ef0a7c61000fad8b74be20
Stefan Vacareanu <stefan@codacy.com>
2024-06-05 10:28:44 +0300

    Add extra blacklist

--- internal/docgen/parsing.go
+++ internal/docgen/parsing.go
@@ -282,7 +282,7 @@ func (rs SemgrepRules) toPatternWithExplanation(defaultRules SemgrepRules) Patte
 	pwes := make(PatternsWithExplanation, len(rs))
 
 	for i, r := range rs {
-		if r.ID != "rules_lgpl_oc_other_rule-ios-self-signed-ssl" {
+		if r.ID != "rules_lgpl_oc_other_rule-ios-self-signed-ssl" && r.ID != "java_deserialization_rule-JacksonUnsafeDeserialization" {
 			pwes[i] = r.toPatternWithExplanation(defaultRules)
 		}
 	}
