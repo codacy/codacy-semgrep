4e0ebd695b984bb7beb85b24f6e4aaf6a1f99a87
Hélio Rocha <helio@codacy.com>
2024-10-16 12:26:59 +0100

    add pattern for hardcoded java passwords

---
+++ docs/multiple-tests/codacy-rules-java/src/Program.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+
+class Program
+    {
+        public static void main(String[] args)
+        {
+            private static final String PASSWORD = "password" ; // Issue: Hardcoded password
+
+            System.out.println("This is a security risk: " + PASSWORD);
+        }
+    }
+
