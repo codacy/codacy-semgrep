3c63a0e9e662d4c036fbfef5ec68c031cbedcca7
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2025-01-28 14:23:47 +0000

    fix javascript password rule (#40)

--- docs/multiple-tests/codacy-rules-javascript/src/index.js
+++ docs/multiple-tests/codacy-rules-javascript/src/index.js
@@ -1,8 +1,10 @@
 
 function main(args) {
     var PASSWORD = "password"; // Issue: Hardcoded password
-    let salasana = "YAY"
-    const senha = "senha";
+    let salasana = 'YAY'
+    const senha = `senha`;
 
+
+    const letPassword = password();
 }
 
