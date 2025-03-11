5561927f8c6bbcef8a3bfae9340089a287866a09
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2025-03-11 10:21:10 +0000

    Add support for FlexibleSearchQuery on hybris(SAP) (#44)

--- docs/multiple-tests/codacy-rules-java/src/Program.java
+++ docs/multiple-tests/codacy-rules-java/src/Program.java
@@ -1,11 +1,17 @@
 import java.io.Console;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
 
 class Program
     {
         public static void main(String[] args)
         {
             private static final String PASSWORD = "password" ; // Issue: Hardcoded password
+            final FlexibleSearchQuery query = new FlexibleSearchQuery("SELECT {a.pk} FROM {TEST AS a} WHERE {a.uid} ="+ uid +" AND {a.visibleInAddressBook} = true");
 
+            final FlexibleSearchQuery okquery = new FlexibleSearchQuery(
+                "SELECT {a.pk} FROM {TEST AS a} WHERE {a.uid} = ?uid AND {a.visibleInAddressBook} = true"
+            );
+            okquery.addQueryParameter("uid", uid);
             System.out.println("This is a security risk: " + PASSWORD);
         }
     }
