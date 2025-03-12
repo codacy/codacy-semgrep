6757aa055b839d096b639b50c89aa4d036d13301
Hélio Rocha <helio@codacy.com>
2025-03-12 10:16:54 +0000

    Add rule to detect null dereference in c#

--- docs/multiple-tests/codacy-rules/src/codacy-csharp-security-hard-coded-password.cs
+++ docs/multiple-tests/codacy-rules/src/codacy-csharp-security-hard-coded-password.cs
@@ -10,5 +10,39 @@ namespace SecurityExample
 
             Console.WriteLine("This is a security risk: " + password);
         }
+
+        public static bool? IsRegular(bool freqNoneOrNotPeriodic, bool freqPeriodical, IFrequency frequency)
+        {
+            if(frequency != null)
+            {
+                return false;
+            }
+
+  
+
+            if (frequency.Days != 0)
+                return false;
+
+            if (frequency.Years == 1 && frequency.Months == 0)
+                return null;
+
+        }
+
+        public static bool? IsRegular2(bool freqNoneOrNotPeriodic, bool freqPeriodical, IFrequency frequency)
+        {
+            if(frequency == null)
+            {
+                return false;
+            }
+
+  
+
+            if (frequency.Days != 0)
+                return false;
+
+            if (frequency.Years == 1 && frequency.Months == 0)
+                return null;
+
+        }
     }
 }
