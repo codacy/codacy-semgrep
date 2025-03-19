ec581bbbdf51dd73edca997d7cded9575e071ad4
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2025-03-19 14:32:18 +0000

    fix codacy.generic.sql.grant-select-no-role rule (#48)

--- docs/multiple-tests/codacy-rules-sql/src/script.sql
+++ docs/multiple-tests/codacy-rules-sql/src/script.sql
@@ -7,3 +7,13 @@ WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON mydb.* TO myuser;
 
 
+
+
+GRANT SELECT ON mydb.* TO scmdbi;
+
+GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmdbi;
+
+GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmd_dev_role;
+
+
+SELECT fnd_profile.value('JTF_PROFILE_DEFAULT_NUM_ROWS') from dual;
