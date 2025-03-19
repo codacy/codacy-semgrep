4266b694cfb41566660af0dd406077fcecfd707c
Hélio Rocha <helio@codacy.com>
2025-03-19 14:26:04 +0000

    fix fnd-profile-in-query rule

--- docs/multiple-tests/codacy-rules-sql/src/script.sql
+++ docs/multiple-tests/codacy-rules-sql/src/script.sql
@@ -14,3 +14,6 @@ GRANT SELECT ON mydb.* TO scmdbi;
 GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmdbi;
 
 GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmd_dev_role;
+
+
+SELECT fnd_profile.value('JTF_PROFILE_DEFAULT_NUM_ROWS') from dual;
