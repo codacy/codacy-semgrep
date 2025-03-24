366f88b549e93f08676d9ddb5ace4a549c8b8c4f
david.marinho@codacy.com <david.marinho@codacy.com>
2025-03-24 12:09:48 +0000

    improve camelcase rule

--- docs/multiple-tests/codacy-rules/codacy-rules-csharp/src/codacy.csharp-camelcase.cs
+++ docs/multiple-tests/codacy-rules/codacy-rules-csharp/src/codacy.csharp-camelcase.cs
@@ -2,6 +2,10 @@ class myClass
 {
     public void MyMethod() 
     {
-        int MyVariable = 10; 
+        int MyVariable = 10;
+        List<int> numberList = new List<int>();
+        Dictionary<string, int> ageMap = new Dictionary<string, int>();
+        MyClass instance = new MyClass();
+        const int maxLimit = 100;
     }
 }
