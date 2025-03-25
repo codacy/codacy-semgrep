f1660072dd241964e998bf783a4d44607cf1c237
david.marinho@codacy.com <david.marinho@codacy.com>
2025-03-25 15:03:53 +0000

    change camelcase to lowercase model

--- docs/multiple-tests/codacy-rules-csharp/src/codacy.csharp-lowercase.cs
+++ docs/multiple-tests/codacy-rules-csharp/src/codacy.csharp-lowercase.cs
@@ -2,8 +2,8 @@ class myClass
 {
     public void MyMethod() 
     {
-        int MyVariable = 10;
-        bool anothervariable = true;
+        int Myvariable = 10;
+        bool Another9Variable = true;
         int minLimit = 10 //this shouldn't be flagged
         // int maxLimit = 100; this shouldn't be flagged
     }
