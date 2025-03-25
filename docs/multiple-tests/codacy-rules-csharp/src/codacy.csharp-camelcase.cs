dd3a46b4c076c04fb77386584ce35d0d2596e064
david.marinho@codacy.com <david.marinho@codacy.com>
2025-03-25 09:12:42 +0000

    fix camelcase rule

--- docs/multiple-tests/codacy-rules-csharp/src/codacy.csharp-camelcase.cs
+++ docs/multiple-tests/codacy-rules-csharp/src/codacy.csharp-camelcase.cs
@@ -3,12 +3,8 @@ class myClass
     public void MyMethod() 
     {
         int MyVariable = 10;
-        List<int> numberList = new List<int>();
-        Dictionary<string, int> ageMap = new Dictionary<string, int>();
-        MyClass instance = new MyClass();
-        const int maxLimit = 100;
-
-        // const int maxLimit = 100; this shouldn't be flagged
-        // public void MyMethod() {}
+        bool anothervariable = true;
+        int minLimit = 10 //this shouldn't be flagged
+        // int maxLimit = 100; this shouldn't be flagged
     }
 }
