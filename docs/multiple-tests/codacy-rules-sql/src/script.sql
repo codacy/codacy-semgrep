5f1ad3a9d5b3cfd5a956c6945f75cef0f828a8cf
david.marinho@codacy.com <david.marinho@codacy.com>
2025-05-16 17:36:23 +0100

    fix tests

--- docs/multiple-tests/codacy-rules-sql/src/script.sql
+++ docs/multiple-tests/codacy-rules-sql/src/script.sql
@@ -18,9 +18,9 @@ GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmd_dev_role;
 
 SELECT fnd_profile.value('JTF_PROFILE_DEFAULT_NUM_ROWS') from dual;
 
-SELECT * from table_name where lang = "US";
-SELECT * from table_name where currency = "USD"; 
-SELECT * from table_name where org_id = 123;
+SELECT * FROM users WHERE language = 'US';
+UPDATE accounts SET currency := 'USD';
+UPDATE customers SET customers.org_id := '12345';
 
 SELECT * FROM something WHERE lookup_type = 'ABC';
 SELECT * FROM apps.fnd_lookup_values;
