167bf2b4140edcc016aa60289695c9914d8d8485
DMarinhoCodacy <102817200+DMarinhoCodacy@users.noreply.github.com>
2025-05-19 10:24:28 +0100

    add sql rules (#54)

--- docs/multiple-tests/codacy-rules-sql/src/script.sql
+++ docs/multiple-tests/codacy-rules-sql/src/script.sql
@@ -17,3 +17,12 @@ GRANT DELETE, INSERT, SELECT, UPDATE ON mydb.* TO scmd_dev_role;
 
 
 SELECT fnd_profile.value('JTF_PROFILE_DEFAULT_NUM_ROWS') from dual;
+
+SELECT * FROM users WHERE language = 'US';
+UPDATE accounts SET currency := 'USD';
+UPDATE customers SET customers.org_id := '12345';
+
+SELECT * FROM something WHERE lookup_type = 'ABC';
+SELECT * FROM apps.fnd_lookup_values;
+
+SELECT * FROM ap_all_invoices WHERE invoice_date > SYSDATE - 30;
