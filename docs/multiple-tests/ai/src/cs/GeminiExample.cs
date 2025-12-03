8d20fbe8e227193102ae1416d8d0fd2e97794c66
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2025-12-03 16:51:29 +0000

    Convert the codacy.csharp.ai.insecure-llm-model-usage rule to support allow list (#75)

--- docs/multiple-tests/ai/src/cs/GeminiExample.cs
+++ docs/multiple-tests/ai/src/cs/GeminiExample.cs
@@ -7,6 +7,9 @@ public class GenerateContentSimpleText {
     // The client gets the API key from the environment variable `GEMINI_API_KEY`.
     var client = new Client();
     var response = await client.Models.GenerateContentAsync(
+      model: "deepseek-v3.2", contents: "Explain how AI works in a few words"
+    );
+    var response2 = await client.Models.GenerateContentAsync(
       model: "gemini-2.5-flash", contents: "Explain how AI works in a few words"
     );
     Console.WriteLine(response.Candidates[0].Content.Parts[0].Text);
